c47cc8bc3e05dfd912af4e6622d8fbc4a662238f
jicrbuh <jicrbuh1@gmail.com>
2018-07-14 17:18:27 +0300

    added to sudoku_board.c sudoku_board.h

---
+++ sudoku_board.c
@@ -0,0 +1,52 @@
+#include "sudoku_board.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Board* createBoard(int blockHeight, int blockLength, int** mtx) {
+
+	Board board = (Board *) calloc(sizeof(Board));
+	if (board == NULL) {
+		return NULL;
+	}
+	board->block_height=blockHeight;
+	board->block_length=blockLength;
+
+	int boardSize = blockHeight*blockLength;
+	mtx = calloc(boardSize, 1+sizeof(int*)); /* alloc one extra ptr*/
+	if (mtx == NULL) {
+		return NULL;
+	}
+	for(int i = 0;i<boardSize;i++) {
+		mtx[i] = calloc(boardSize, sizeof(int));
+	}
+	mtx[boardSize] = NULL; /* set the extra ptr to NULL*/
+	for(int i = 0;i<boardSize;i++) {
+		for(int j = 0;j<boardSize;j++) {
+			mtx[i][j] = i*boardSize+j;
+		}
+	}
+	board.matrix=mtx;
+
+	return board;
+
+	return NULL;
+}
+Board* copyBoard(Board* board){
+	return NULL;
+}
+void destroyBoard(Board* board){
+
+	for( int i=0; board[i] != NULL; i++ ) {
+	    free( board[i] );
+	}
+	free(board->matrix);
+	free(board->block_height); /*maybe can't free those because they aren't pointers?*/
+	free(board->block_length);
+}
+void printBoard(Board* board){
+
+}
+void printBoard(Board* board){
+
+}
---
+++ sudoku_board.h
@@ -0,0 +1,23 @@
+/*
+ * sudoku_board.h
+ *
+ *  Created on: 14 Jul 2018
+ *      Author: chen
+ */
+
+#ifndef SUDOKU_BOARD_H_
+#define SUDOKU_BOARD_H_
+
+typedef struct board_t {
+	int block_height;
+	int block_length;
+	int** matrix;
+} Board;
+
+Board* createBoard(int block_height, int block_length, int** matrix);
+Board* copyBoard(Board* board);
+void destroyBoard(Board* board);
+void printBoard(Board* board);
+void printBoard(Board* board);
+
+#endif /* SUDOKU_BOARD_H_ */
