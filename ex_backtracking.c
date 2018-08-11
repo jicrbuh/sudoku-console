cb3d7023f42b4f1168cd3a97cc3e8d7a50ec8dfb
jicrbuh <jicrbuh1@gmail.com>
2018-08-11 21:42:35 +0300

    added ex_backtracking.c and ex_backtracking.h

---
+++ ex_backtracking.c
@@ -0,0 +1,8 @@
+#include "sudoku_board.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isCellErroneous(Board* board, int x, int y) {
+	return 0;
+}
---
+++ ex_backtracking.h
@@ -0,0 +1,13 @@
+/*
+ * ex_backtracking.h
+ *
+ *  Created on: 11 Aug 2018
+ *      Author: chen
+ */
+
+#ifndef EX_BACKTRACKING_H_
+#define EX_BACKTRACKING_H_
+
+
+
+#endif /* EX_BACKTRACKING_H_ */
